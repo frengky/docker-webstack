4f549f5b880b16be73b8f5b5aab5406977e8154a
Frengky <frengky.lim@gmail.com>
2020-12-01 21:13:34 +0700

    refactor dbgp

---
+++ scripts/docker-entrypoint.dbgp.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ "$1" = "supervisord" ]; then
+
+    MAIL_HOST=${MAIL_HOST:-mailserver}
+    MAIL_PORT=${MAIL_PORT:-3025}
+    SENDMAIL_PATH="/usr/sbin/sendmail -S ${MAIL_HOST}:${MAIL_PORT} -t -i"
+
+    sed -i "s|;*sendmail_path =.*|sendmail_path=\"${SENDMAIL_PATH}\"|i" /etc/php7/php.ini
+
+    printf "\
+; Uncomment to enable this extension.\n\
+zend_extension=xdebug.so\n\
+xdebug.remote_enable=1\n\
+xdebug.remote_host=127.0.0.1\n\
+xdebug.remote_port=9010\n\
+xdebug.remote_connect_back=0\n\
+xdebug.remote_autostart=1\n" > /etc/php7/conf.d/xdebug.ini
+
+fi
+
+exec "$@"
