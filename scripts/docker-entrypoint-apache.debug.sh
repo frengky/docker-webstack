7f36df70911c7e3324a100f9cdeac4b59ae728a9
Frengky <frengky.lim@gmail.com>
2020-12-03 18:05:11 +0700

    configurable xdebug remote port

--- scripts/docker-entrypoint-apache.debug.sh
+++ scripts/docker-entrypoint-apache.debug.sh
@@ -8,12 +8,13 @@ if [ "$1" = "httpd" ]; then
     sed -i "s#Timeout\ 60#Timeout\ 600#" /etc/apache2/httpd.conf
 
     XDEBUG_REMOTE_HOST=${XDEBUG_REMOTE_HOST:-`ip r | awk '/default/{print $3}'`}
+    XDEBUG_REMOTE_PORT=${XDEBUG_REMOTE_PORT:-9000}
     printf "\
 ; Uncomment to enable this extension.\n\
 zend_extension=xdebug.so\n\
 xdebug.remote_enable=1\n\
 xdebug.remote_host=${XDEBUG_REMOTE_HOST}\n\
-xdebug.remote_port=9000\n\
+xdebug.remote_port=${XDEBUG_REMOTE_PORT}\n\
 xdebug.remote_connect_back=0\n\
 xdebug.remote_autostart=1\n" > /etc/php7/conf.d/xdebug.ini
 
--- scripts/docker-entrypoint.debug.sh
+++ scripts/docker-entrypoint.debug.sh
@@ -9,12 +9,13 @@ if [ "$1" = "supervisord" ]; then
     sed -i "s|;*sendmail_path =.*|sendmail_path=\"${SENDMAIL_PATH}\"|i" /etc/php7/php.ini
 
     XDEBUG_REMOTE_HOST=${XDEBUG_REMOTE_HOST:-`ip r | awk '/default/{print $3}'`}
+    XDEBUG_REMOTE_PORT=${XDEBUG_REMOTE_PORT:-9000}
     printf "\
 ; Uncomment to enable this extension.\n\
 zend_extension=xdebug.so\n\
 xdebug.remote_enable=1\n\
 xdebug.remote_host=${XDEBUG_REMOTE_HOST}\n\
-xdebug.remote_port=9000\n\
+xdebug.remote_port=${XDEBUG_REMOTE_PORT}\n\
 xdebug.remote_connect_back=0\n\
 xdebug.remote_autostart=1\n" > /etc/php7/conf.d/xdebug.ini
 
