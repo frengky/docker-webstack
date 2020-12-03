03393d833f57d89b1ac7c9ca82cd5686f22dea98
Frengky <frengky.lim@gmail.com>
2020-12-03 11:52:12 +0700

    make xdebug remote host configurable

--- scripts/docker-entrypoint-apache.debug.sh
+++ scripts/docker-entrypoint-apache.debug.sh
@@ -7,12 +7,12 @@ if [ "$1" = "httpd" ]; then
     sed -i "s#/var/www/localhost/htdocs#${DOCUMENT_ROOT:-/app}#" /etc/apache2/httpd.conf
     sed -i "s#Timeout\ 60#Timeout\ 600#" /etc/apache2/httpd.conf
 
-    XDEBUG_IDE_HOST=$(ip r | awk '/default/{print $3}')
+    XDEBUG_REMOTE_HOST=${XDEBUG_REMOTE_HOST:-`ip r | awk '/default/{print $3}'`}
     printf "\
 ; Uncomment to enable this extension.\n\
 zend_extension=xdebug.so\n\
 xdebug.remote_enable=1\n\
-xdebug.remote_host=${XDEBUG_IDE_HOST}\n\
+xdebug.remote_host=${XDEBUG_REMOTE_HOST}\n\
 xdebug.remote_port=9000\n\
 xdebug.remote_connect_back=0\n\
 xdebug.remote_autostart=1\n" > /etc/php7/conf.d/xdebug.ini
--- scripts/docker-entrypoint.debug.sh
+++ scripts/docker-entrypoint.debug.sh
@@ -8,12 +8,12 @@ if [ "$1" = "supervisord" ]; then
 
     sed -i "s|;*sendmail_path =.*|sendmail_path=\"${SENDMAIL_PATH}\"|i" /etc/php7/php.ini
 
-    XDEBUG_IDE_HOST=$(ip r | awk '/default/{print $3}')
+    XDEBUG_REMOTE_HOST=${XDEBUG_REMOTE_HOST:-`ip r | awk '/default/{print $3}'`}
     printf "\
 ; Uncomment to enable this extension.\n\
 zend_extension=xdebug.so\n\
 xdebug.remote_enable=1\n\
-xdebug.remote_host=${XDEBUG_IDE_HOST}\n\
+xdebug.remote_host=${XDEBUG_REMOTE_HOST}\n\
 xdebug.remote_port=9000\n\
 xdebug.remote_connect_back=0\n\
 xdebug.remote_autostart=1\n" > /etc/php7/conf.d/xdebug.ini
